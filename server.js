87f10e1c7361f170d0ab81d782b972e928ed4120
akash-jan-aj <ashakash909@gmail.com>
2026-03-03 13:47:16 +0530

    new commit added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Ash Bro 🚀 CI/CD Learning!");
+  res.send("Hello Ash Bro 🚀 CI/CD Learning version 2!");
 });
 
 app.listen(3000, () => {
